7fa2a6480d1efa0db4e55c03c67a8d145a7c73ba
Omkar Tupere <115610759+omkar7176@users.noreply.github.com>
2025-08-26 17:34:30 +0000

    global variables

---
+++ 4_pythonglobalvariables.py
@@ -0,0 +1,13 @@
+# defining global variable
+a = "Omkar"
+
+def myfunction():
+    print("My name is", a)
+
+myfunction()
+
+# defining global variable
+def name():
+    x = "Kunal"
+    print("My name is", x)
+name()    
