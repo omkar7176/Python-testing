3ae4b7b605a0d6fd58cc33496b32d4a89a1e1aa1
Omkar Tupere <115610759+omkar7176@users.noreply.github.com>
2025-08-28 04:33:09 +0000

    loop with tuples

---
+++ 15_looptuples.py
@@ -0,0 +1,15 @@
+lang = ("Python", "Java", "JavaScript")
+for i in lang:
+    print(i)
+
+# Loop through the index
+fruit = ["mango", "grapes", "watermelon", "cherry", "banana", "orange", "kiwi"]
+# for i in range(len(fruit)):
+#     print(fruit[i])
+
+# Using it while loop
+fruit = ["mango", "grapes", ]
+i = 0
+while i < len(fruit):
+    print(fruit[i])
+    i = i + 1
