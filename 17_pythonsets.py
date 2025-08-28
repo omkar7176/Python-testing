9fe98437cbd2af9ab97d72eb00afced1b4280268
Omkar Tupere <115610759+omkar7176@users.noreply.github.com>
2025-08-28 06:51:10 +0000

    Python sets

---
+++ 17_pythonsets.py
@@ -0,0 +1,13 @@
+'''
+Sets: Ordered❌ || Indexed❌ || Duplicates Allowed❌ ||
+      Heterogeneous Elements ✅ || Mutable✅
+    : It contains the Unique items.
+    : add the items in set, we use {}.
+'''
+
+# What are sets?
+a = {"apple", "cherry", "orange"}
+print(a)
+print(type(a))
+
+# duplicates are not allowed
