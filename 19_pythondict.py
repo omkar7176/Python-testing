4b15d4fee85aa7b3f69930f81797802515ffc8de
Omkar Tupere <115610759+omkar7176@users.noreply.github.com>
2025-08-28 10:25:57 +0000

    Python Dictionary

---
+++ 19_pythondict.py
@@ -0,0 +1,14 @@
+'''
+Dictionary: Ordered✅ || Indexed✅ || Mutable✅ || Heterogeneous Elements ✅ ||
+            Duplicates Allowed❌(keys must be unique)
+           : we store the items in dict in key:value pairs.
+           : make sure keys must be unique, values can duplicates. 
+'''
+# Dictionary
+emp1 = {
+    "Name": "Rahul",
+    "Age": 22,
+    "Position": "Developer",
+    "Country" : "India"
+}
+print(type(emp1))
