e8f9f59658323f54d62fd486a23d38e3a85f2ffc
Omkar Tupere <115610759+omkar7176@users.noreply.github.com>
2025-08-29 09:02:38 +0000

    Python Array(List)

---
+++ 26_pythonarray.py
@@ -0,0 +1,8 @@
+# Arrays are used to store multiple values in one single variables.
+'''
+NOTE:
+Without the array module or NumPy, Python does not have a built-in, low-level array type. Instead, we use the built-in list type to achieve array-like functionality.
+'''
+lang = ["Python", "Java", "JavaScript"]
+print(lang)
+print(type(lang))
